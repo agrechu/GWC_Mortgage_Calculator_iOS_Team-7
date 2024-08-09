ee77afcbc98edaad953950fed130a21ace1d3a84
andreacalderonn <anncalderonn3@gmail.com>
2024-08-08 17:22:12 -0700

    Add files via upload

---
+++ MortgageCalculator/MortgageCalculator/makeAccountViewController.swift
@@ -0,0 +1,41 @@
+//
+//  makeAccountViewController.swift
+//  MortgageCalculator
+//
+//  Created by Andrea Calderon on 8/8/24.
+//
+
+import UIKit
+
+class makeAccountViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameText: UITextField!
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
+    @IBOutlet weak var confirmText: UITextField!
+    
+    @IBOutlet weak var register: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
